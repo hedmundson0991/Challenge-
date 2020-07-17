281528ce2fff7661d87baca2209bc5993de47d80
unknown <hedmundson.he@gmail.com>
2020-07-17 14:36:14 -0400

    html js css

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ var x = "laughing";
 var r = false;
 var y = 195;
 var h = "30";
-var k = "smiling"
-alert("robin is" + k + x)
-alert("work is always easy" + r)
-alert("how many countries are in the world" + y)
-alert("my birthday was last month and I turned" + h)
+var k = "smiling";
+alert("robin is " + k + " and " + x);
+alert("work is always easy " + r);
+alert("how many countries are in the world " + y);
+alert("my birthday was last month and I turned " + h);
 var schoolsupplies = ["pens", "paper", "crayons", "glue", "scissors", "pencil box"];
-console.log(schoolsupplies[schoolsupplies.length - 1]);
-console.log(schoolsupplies.indexOf("paper"));
-console.log(schoolsupplies[schoolsupplies.length - 2]);
-console.log(schoolsupplies.indexOf("pens"));
-console.log(schoolsupplies.indexOf("scissors"));
-console log(schoolsupplies.indexOf("glue"));
-console log(schoolsupplies.indexOf("crayons");
+console.log(schoolsupplies[1])
+console.log(schoolsupplies[3])
+console.log(schoolsupplies[5])
+console.log(schoolsupplies[4])
+console.log(schoolsupplies.length)
+schoolsupplies.slice(3)
+console.log(schoolsupplies.length)
