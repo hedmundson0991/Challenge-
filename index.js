455ac6f258429304bab0030d35dfd1516499d0ae
unknown <hedmundson.he@gmail.com>
2020-07-17 11:35:23 -0400

    added html js css

---
+++ index.js
@@ -0,0 +1,17 @@
+var x = "laughing";
+var r = false;
+var y = 195;
+var h = "30";
+var k = "smiling"
+alert("robin is" + k + x)
+alert("work is always easy" + r)
+alert("how many countries are in the world" + y)
+alert("my birthday was last month and I turned" + h)
+var schoolsupplies = ["pens", "paper", "crayons", "glue", "scissors", "pencil box"];
+console.log(schoolsupplies[schoolsupplies.length - 1]);
+console.log(schoolsupplies.indexOf("paper"));
+console.log(schoolsupplies[schoolsupplies.length - 2]);
+console.log(schoolsupplies.indexOf("pens"));
+console.log(schoolsupplies.indexOf("scissors"));
+console log(schoolsupplies.indexOf("glue"));
+console log(schoolsupplies.indexOf("crayons");
